8fd11800449f6b1a50b4a50a6fe0c3f067a754f0
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-08-21 14:00:07 -0300

    pruebas jest

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,7 +1,15 @@
-test("Esta prueba no puede fallar", () =>{
+describe("Pruebas en <DemoComponent />", () => {
+    test("Esta prueba no puede fallar", () =>{
+        //1 Inicializacion
+        const message1 = "Hola mundo";
 
-    if (0===1){
-        throw new error("no pude dividir entre cero");
-        
-    }
+        //2 estimulo
+        const message2 = message1.trim();
+    
+        //3 observar el comportamiento.. esperado
+        expect( message1 ).toBe( message2 );
+    });
+
 });
+
+
