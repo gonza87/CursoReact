4b9eba72091c91742600fa0424371842a1d73a22
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-08-28 11:11:54 -0300

    test

--- src/base-pruebas/02-template-string.js
+++ src/base-pruebas/02-template-string.js
@@ -6,4 +6,3 @@ export function getSaludo(nombre) {
     return 'Hola ' + nombre;
 }
 
-console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
--- tests/base-pruebas/02-template-string.test.js
+++ tests/base-pruebas/02-template-string.test.js
@@ -1,5 +1,10 @@
+import { getSaludo } from "../../src/base-pruebas/02-template-string";
+
 describe("Pruebas en el archivo 02-template-string", () =>{
     test('getSaludo debe de retornar "Hola Fernando"', () => {
-
+        const nombre = 'Fernando';
+        const message = getSaludo (nombre);
+        
+        expect (message).toBe(`Hola ${nombre}`)
     });
 })
